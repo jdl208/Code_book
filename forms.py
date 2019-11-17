3a3dceb91cf39b72c3d5d24afb9055e057f0ede0
Johan de Leeuw <johandeleeuw@gmail.com>
2019-11-17 19:26:41 +0000

    Added register functionality and route

--- forms.py
+++ forms.py
@@ -2,8 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=5, max =30)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=30)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
--- run.py
+++ run.py
@@ -10,5 +10,14 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'alert_success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+    
+
 if __name__ == '__main__':
     app.run(debug=True)

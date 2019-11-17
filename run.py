e857d956377f6e7aef610ead090d57e1dbc3c3c4
Johan de Leeuw <johandeleeuw@gmail.com>
2019-11-17 21:16:27 +0000

    connected to mongodb

--- run.py
+++ run.py
@@ -1,21 +1,33 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, flash
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from forms import RegistrationForm, LoginForm
+import env  # delete this line when deploying to heroku.
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', users=mongo.db.users.find())
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        users = mongo.db.users
+        users.insert_one({'username': form.username.data,
+                          'email': form.email.data,
+                          'password': form.password.data})
         flash(f'Account created for {form.username.data}!', 'alert_success')
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
@@ -32,4 +44,6 @@ def login():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=os.environ.get('IP', '0.0.0.0'),
+            port=int(os.environ.get('PORT', 5000)),
+            debug=True)

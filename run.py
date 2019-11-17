c3fddc6386dedcc6764c9dc4f4f86fa279766c4a
Johan de Leeuw <johandeleeuw@gmail.com>
2019-11-17 19:27:58 +0000

    Added login functionality and route

--- run.py
+++ run.py
@@ -17,7 +17,19 @@ def register():
         flash(f'Account created for {form.username.data}!', 'alert_success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
-    
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'test@login.com' and form.password.data == 'password':
+            flash('Login succesfull!', 'alert_success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login failed, please check your email and/or password!', 'alert_fail')
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)

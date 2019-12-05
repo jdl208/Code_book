39b5665a950959078792b31fd3abcf1f9e0253e9
Johan de Leeuw <johandeleeuw@gmail.com>
2019-12-05 21:04:32 +0000

    Set debug to false

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from codebook import app
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', "0.0.0.0"),
             port=int(os.environ.get('PORT', "5000")),
-            debug=True)
+            debug=False)
